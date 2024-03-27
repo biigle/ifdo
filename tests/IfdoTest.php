baba437a382c57bc1ebdc534188c303ab9720ad0
Christoph Bach <christoph-bach@gmx.de>
2024-03-27 17:14:28 +0100

    add more examples

--- tests/IfdoTest.php
+++ tests/IfdoTest.php
@@ -6,10 +6,24 @@ use PHPUnit\Framework\TestCase;
 
 class IfdoTest extends TestCase
 {
-	public function testReadFromFile()
-	{
-		$obj = Ifdo::fromFile(__DIR__ . '/fixtures/ifdo-test-v2.0.0.json');
-		$obj->setDebug(true);
-		$this->assertEquals(true, $obj->isValid());
-	}
+    public function testReadFromFile()
+    {
+        $obj = Ifdo::fromFile(__DIR__ . '/fixtures/ifdo-test-v2.0.0.json');
+        $obj->setDebug(true);
+        $this->assertEquals(true, $obj->isValid());
+    }
+
+    public function testImageExample()
+    {
+        $obj = Ifdo::fromFile(__DIR__ . '/fixtures/image-example-1.json');
+        $obj->setDebug(true);
+        $this->assertEquals(true, $obj->isValid());
+    }
+
+    public function testVideoExample()
+    {
+        $obj = Ifdo::fromFile(__DIR__ . '/fixtures/video-example-1.json');
+        $obj->setDebug(true);
+        $this->assertEquals(true, $obj->isValid());
+    }
 }
